77c5357fe8f0893f2377ffb40509677cd0e4489f
snowman <xxxyukixconanxmysteryxxx@yahoo.co.jp>
2021-01-10 16:20:56 +0900

    [勤怠]初期構築
    dotenvを追加

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,8 +1,4 @@
-const { setHeadlessWhen } = require('@codeceptjs/configure');
-
-// turn on headless mode when running with HEADLESS=true environment variable
-// export HEADLESS=true && npx codeceptjs run
-setHeadlessWhen(process.env.HEADLESS);
+require('dotenv').config()
 
 exports.config = {
   tests: './*_test.js',
